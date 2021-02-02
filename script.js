da9f3c24d69b37baa6ba9db64e7b637224084bdd
jonathanyu1 <jonathanyusir@gmail.com>
2021-02-01 21:01:24 -0500

    added gameboard styling, added player object, added click functionality, added check win condition

--- script.js
+++ script.js
@@ -1,46 +1,111 @@
-const game = (() => {
-
-    return {
-        
-    };
-})();
-
-
 const gameBoard = (() => {
     // select dom element
-    const boardContainer = document.getElementsByClassName('boardContainer');
-    const createGrid = () =>{
+    const boardContainer = document.querySelector('.boardContainer');
+    const board=[];
+
+    const createBoard = () =>{
         for (i=0;i<9;i++){
             console.log(i);
             const cell=document.createElement('div');
             cell.classList.add('cell');
             cell.id = `cell-${i}`;
-            boardContainer[0].appendChild(cell);
+            boardContainer.appendChild(cell);
+            board[i]=cell;
+            // cell.addEventListener('click', function(e){
+            //     console.log(e.target);
+            // });
         }
     }
+
+    const getBoard = () =>{
+        return board;
+    }
+
     const removeCells = () =>{
         while (boardContainer.hasChildNodes()){
-            gridContainer.removeChild(gridContainer.lastChild);
+            boardContainer.removeChild(boardContainer.lastChild);
         }
     }
-    const clearGrid = () =>{
-        // delete grid and recreate
+    const clearBoard = () =>{
+        // delete board and recreate
         removeCells();
-        createGrid();
+        createBoard();
     }
     return {
-        boardContainer: boardContainer[0],
-        createGrid,
-        clearGrid
+        boardContainer: boardContainer,
+        createBoard,
+        clearBoard,
+        getBoard
     }
-
 })();
 
-const player = () => {
-
+const player = (mark) => {
+    const getMark = () => mark;
+    return {
+        getMark
+    }
 };
 
+const game = (() => {
+    // initialize board, players
+    let turn=1;
+    const winCon = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    gameBoard.createBoard();
+    const board = gameBoard.getBoard();
 
-// global calls to objects
+    const playerOne = player('X');
+    const playerTwo = player('O');
+    console.log(playerOne.getMark());
+    console.log(playerTwo.getMark());
 
-gameBoard.createGrid();
+    const checkWin = () => {
+        console.log(board);
+        winCon.forEach((condition)=>{
+            // console.log(condition);
+            // console.log(condition[0]);
+            // console.log(condition[1]);
+            // console.log(condition[2]);
+            // check if matches win condition, and makes sure not empty 
+            if (board[condition[0]].innerHTML !== '' 
+            && (board[condition[0]].innerHTML===board[condition[1]].innerHTML 
+            && board[condition[0]].innerHTML===board[condition[2]].innerHTML)){
+                console.log(`winner is ${board[condition[0]].innerHTML}`);
+            }
+        });
+    };
+
+    board.forEach((cell)=>{
+        cell.addEventListener('click', function(e){
+            console.log(e.target);
+            // prevent same cell from being clicked
+            if (e.target.innerHTML !== ''){
+                console.log('cannot');
+                return;
+            }
+            if (turn%2){
+                e.target.innerHTML=playerOne.getMark();
+                console.log('X');
+            } else {
+                e.target.innerHTML=playerTwo.getMark();
+                console.log('O');
+            }
+            turn++;
+            console.log(turn);
+            // check win
+            checkWin();
+        });
+    });
+
+    return {
+
+    };
+})();
